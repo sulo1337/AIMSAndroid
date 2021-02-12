bc3d2d835225635d4c29a9c5ceb5d3c166d64058
sulo1337 <s.acharya1337@gmail.com>
2021-02-11 22:22:07 -0600

    implemented view model factory -- refactoring

--- app/src/main/java/com/example/aimsandroid/homefragment/HomeFragment.kt
+++ app/src/main/java/com/example/aimsandroid/homefragment/HomeFragment.kt
@@ -24,15 +24,25 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentHomeBinding.inflate(inflater)
-        return binding.root
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        //set action bar title
         (activity as AppCompatActivity?)!!.supportActionBar!!.title = getString(R.string.homeFragmentToolbarTitle)
-//        val locationManager: LocationManager = (activity as AppCompatActivity?)!!.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        //obtain databinding
+        val binding = FragmentHomeBinding.inflate(inflater)
+
+        //obtain location manager
+        val locationManager: LocationManager = (activity as AppCompatActivity?)!!.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        //inject location manager to view model factory
+        val viewModelFactory: HomeViewModelFactory = HomeViewModelFactory(locationManager)
+
+        //generate view model using the factory
+        viewModel = ViewModelProvider(this, viewModelFactory).get(HomeViewModel::class.java)
+
+        //enable live data to be bound from viewmodel to ui
+        binding.lifecycleOwner = this
+
+        return binding.root
     }
 
 }
