0d0bfdeb89fcdfa03a1bfa20fe49690ba3aa03e0
sulo1337 <s.acharya1337@gmail.com>
2021-02-11 20:23:49 -0600

    custom toolbar

--- app/src/main/java/com/example/aimsandroid/homefragment/HomeFragment.kt
+++ app/src/main/java/com/example/aimsandroid/homefragment/HomeFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.example.aimsandroid.R
 
 class HomeFragment : Fragment() {
@@ -26,7 +27,7 @@ class HomeFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
+        (activity as AppCompatActivity?)!!.supportActionBar!!.title = getString(R.string.homeFragmentToolbarTitle)
     }
 
 }
