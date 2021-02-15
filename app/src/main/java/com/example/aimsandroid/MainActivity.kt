b38ccfd9e0717a75cd252c1a03171c04d15e9109
calebdang <calebtdang@gmail.com>
2021-02-15 05:00:55 -0600

    implemented permission manager

--- app/src/main/java/com/example/aimsandroid/MainActivity.kt
+++ app/src/main/java/com/example/aimsandroid/MainActivity.kt
@@ -1,12 +1,73 @@
 package com.example.aimsandroid
 
+import android.Manifest
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.ContextThemeWrapper
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.jakewharton.processphoenix.ProcessPhoenix
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        //if we do not have location permission
+        if(ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestLocationPermission()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 1) {
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //if permission is granted
+                restartApp()
+            }
+        }
+
+    }
+
+    private fun restartApp() {
+        ProcessPhoenix.triggerRebirth(applicationContext)
+    }
+
+    private fun requestLocationPermission() {
+        //if requesting location permission second time
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+            AlertDialog.Builder(ContextThemeWrapper(this, R.style.dialog))
+                .setTitle("Permission needed")
+                .setMessage("Location permission is need to access your current location")
+                .setPositiveButton("OK", DialogInterface.OnClickListener() { dialogInterface: DialogInterface, i: Int ->
+                    val permissions: Array<String> = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+                    ActivityCompat.requestPermissions(this, permissions, 1)
+                    dialogInterface.dismiss()
+                })
+                .setNegativeButton("Cancel", DialogInterface.OnClickListener(){ dialogInterface: DialogInterface, i: Int ->
+                    dialogInterface.dismiss()
+                })
+                .create()
+                .show()
+        }
+        //if requesting location permission first time
+        else {
+            val permissions: Array<String> = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+            ActivityCompat.requestPermissions(this, permissions, 1)
+        }
     }
 }
