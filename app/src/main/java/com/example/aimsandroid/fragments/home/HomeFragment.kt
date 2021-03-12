909a968683be6c703384418a470d85aac5a9eb2b
shresthabibek <bibekshrestha101@gmail.com>
2021-03-12 04:21:33 -0600

    made bottom sheet on home screen for current trips

--- app/src/main/java/com/example/aimsandroid/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/aimsandroid/fragments/home/HomeFragment.kt
@@ -6,12 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.FragmentManager
 import com.example.aimsandroid.R
 import com.example.aimsandroid.databinding.FragmentHomeBinding
 import com.example.aimsandroid.utils.OnBottomSheetCallbacks
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class HomeFragment : Fragment() {
 
@@ -20,14 +23,26 @@ class HomeFragment : Fragment() {
     }
 
     private lateinit var viewModel: HomeViewModel
+    private lateinit var sheetBehavior: BottomSheetBehavior<LinearLayout>
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentHomeBinding.inflate(inflater);
+        binding = FragmentHomeBinding.inflate(inflater);
         val toolbarTitle = activity?.findViewById<TextView>(R.id.toolbarTitle) as TextView;
         toolbarTitle.setText(getString(R.string.current_trip_toolbar_title));
+        val backdropHeader = binding.backdropHeader
+        val contentLayout = binding.contentLayout
+        sheetBehavior = BottomSheetBehavior.from(contentLayout)
+        sheetBehavior.isFitToContents = false
+        sheetBehavior.isHideable = false
+        sheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+        backdropHeader.setOnClickListener { it ->
+            toggleFilters()
+        }
         return binding.root;
     }
 
@@ -35,5 +50,15 @@ class HomeFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    private fun toggleFilters() {
+        if(sheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED){
+            sheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            binding.filterIcon.setImageResource(R.drawable.ic_keyboard_arrow_up)
+        } else {
+            binding.filterIcon.setImageResource(R.drawable.ic_keyboard_arrow_down)
+            sheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
     }
 }
