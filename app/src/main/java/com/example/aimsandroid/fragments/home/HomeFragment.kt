d0a3ef95872f493ba2ca3d910cec97ef0f8360b8
sulo1337 <s.acharya1337@gmail.com>
2021-03-14 16:22:51 -0500

    added google maps

--- app/src/main/java/com/example/aimsandroid/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/aimsandroid/fragments/home/HomeFragment.kt
@@ -1,5 +1,8 @@
 package com.example.aimsandroid.fragments.home
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,13 +11,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.FragmentManager
 import com.example.aimsandroid.R
 import com.example.aimsandroid.databinding.FragmentHomeBinding
 import com.example.aimsandroid.utils.OnBottomSheetCallbacks
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.CircleOptions
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import java.lang.Exception
+import kotlin.math.log
 
 class HomeFragment : Fragment() {
 
@@ -25,12 +35,40 @@ class HomeFragment : Fragment() {
     private lateinit var viewModel: HomeViewModel
     private lateinit var sheetBehavior: BottomSheetBehavior<LinearLayout>
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var mapview: MapView
+    private lateinit var map: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater);
+
+        mapview = binding.mapView
+        mapview.onCreate(savedInstanceState)
+//        mapview.getMapAsync { googleMap ->
+//            map = googleMap
+//            try {
+//                MapsInitializer.initialize(requireActivity())
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//            }
+//
+//            val cameraUpdate: CameraUpdate = CameraUpdateFactory.newLatLngZoom(LatLng(43.1, -87.9), 10F)
+//            map.animateCamera(cameraUpdate)
+//        }
+
+        mapview.getMapAsync(OnMapReadyCallback() {
+            it?.let {
+                map = it
+                val coordinates: LatLng = LatLng(32.509312, -92.119301)
+                map.addMarker(MarkerOptions().position(coordinates).title("Maps"))
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 10f))
+                mapview.onResume()
+            }
+        })
+
+
         val backdropHeader = binding.backdropHeader
         val contentLayout = binding.contentLayout
         sheetBehavior = BottomSheetBehavior.from(contentLayout)
@@ -53,10 +91,23 @@ class HomeFragment : Fragment() {
     private fun toggleFilters() {
         if(sheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED){
             sheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-            binding.filterIcon.setImageResource(R.drawable.ic_keyboard_arrow_up)
         } else {
-            binding.filterIcon.setImageResource(R.drawable.ic_keyboard_arrow_down)
             sheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapview.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapview.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapview.onDestroy()
     }
 }
